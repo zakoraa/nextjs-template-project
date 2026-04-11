1ddf53400ae91ed467c6dd32b98f4d128d7134bb
Zakora <raflisilehu20@gmail.com>
2026-04-11 21:09:35 +0700

    fix: add ThemeProvider in layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from 'next';
 import './globals.css';
 import { siteMetadata } from '@/shared/config/metadata';
 import { inter } from '@/shared/config/fonts';
+import { ThemeProvider } from 'next-themes';
 
 
 export const metadata: Metadata = siteMetadata;
@@ -12,9 +13,15 @@ export default function RootLayout({
   children: React.ReactNode;
 }) {
   return (
-    <html className={inter.className}>
+    <html className={inter.className} suppressHydrationWarning>
       <body className="min-h-screen text-text font-sans antialiased">
+         <ThemeProvider
+          attribute="class"
+          defaultTheme="system"
+          enableSystem
+        > 
         {children}
+        </ThemeProvider>
       </body>
     </html>
   );
